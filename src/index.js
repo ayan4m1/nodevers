6cadbb3e24c7e461249c5320e10b6dc008976681
ayan4m1 <andrew@bulletlogic.com>
2024-01-31 22:16:26 -0500

    initial import

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import 'core-js/stable';
+
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import './icons.js';
+
+import './index.scss';
+import App from './components/App';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
